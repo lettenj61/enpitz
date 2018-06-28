132f58e59a2317570b0d6212b030e289bfbb8370
lettenj61 <lettenj61@users.noreply.github.com>
2018-06-28 11:56:46 +0900

    repo: Start.

---
+++ bin/enpitz.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+
+const Config = require('../lib/configure')
+const store = require('../lib/store')
+
+const config = new Config()
+
+program.version('0.1.0')
+
+program.command('add <note>')
+  .description('Save a note to storage')
+  .action(note => {
+    store.addPost(config, note)
+  })
+
+program.command('list [date]')
+  .description('Show notes saved in specific date (default: today)')
+  .action(date => {
+    store.getPosts(config, date)
+  })
+
+program.parse(process.argv)
