2fc99b8ea8199764ac23913bafbf4b1653c36414
lettenj61 <lettenj61@users.noreply.github.com>
2018-06-28 16:05:05 +0900

    app: Render markdown and json

--- bin/enpitz.js
+++ bin/enpitz.js
@@ -11,14 +11,36 @@ program.version('0.1.0')
 
 program.command('add <note>')
   .description('Save a note to storage')
-  .action(note => {
+  .option('-s, --silent', 'Suppress result notification')
+  .action((note, cmd) => {
+    config.silent = cmd.silent
     store.addPost(config, note)
   })
 
 program.command('list [date]')
   .description('Show notes saved in specific date (default: today)')
-  .action(date => {
+  .option('-v, --verbose', 'Print verbose information of notes')
+  .action((date, cmd) => {
+    config.verbose = cmd.verbose
     store.getPosts(config, date)
   })
 
+program.command('render [date]')
+  .description('Render saved notes')
+  .option('-o, --output [file]', 'Write rendered string into given file')
+  .option(
+    '-f, --format [type]',
+    'Specify output format (markdown|md|json)',
+    /^(markdown|md|json)$/i,
+    'markdown'
+  )
+  .action((date, cmd) => {
+    config.format = cmd.format
+    store.renderPosts(config, date)
+  })
+
 program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
